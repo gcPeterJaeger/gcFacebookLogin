749d27027e60ed65e8288ce59e2b5ded79ccce33
Peter Jaeger <peter@jaeger.to>
2018-01-31 21:18:24 +0100

    Projektdateien hinzufügen.

---
+++ gcFacebookLogin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gcFacebookLogin.Startup))]
+namespace gcFacebookLogin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
